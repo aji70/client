b5e0d213e6e64a1546a25f5f989abf9d1ad8a198
Aji <realjaiboi70@gmail.com>
2024-03-30 18:19:47 +0100

    Client

---
+++ src/vite-env.d.ts
@@ -0,0 +1,10 @@
+/// <reference types="vite/client" />
+VITE_API_KEY = "9D2E6X9UM39NEUI1T9UPCGZR9CTGNJF5SN";
+VITE_SECRET_KEY = "ad5d5fe67f1f3cccb0cb";
+VITE_projectId = "912f9a3279905a7dd417a7bf68e04209";
+VITE_ENS_ADDRESS = 0xb903a21f9f7863ced673d8da01f76f0db34f7b15;
+VITE_CHAT_ADDRESS = 0x6420994c197eff88e58ae295f1e3f2f1507a80e6;
+VITE_multicall_address = "0x7a37FcaBaCC6b04422f2A625080cEf2D1B4B6E4e";
+VITE_rpc_url = "https://sepolia.infura.io/v3/f9def3eaa27544bfb40914e2e9d21bb6";
+VITE_wss_rpc_url =
+  "wss://sepolia.infura.io/ws/v3/f9def3eaa27544bfb40914e2e9d21bb6";
